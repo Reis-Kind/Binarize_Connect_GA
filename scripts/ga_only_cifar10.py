a0fd236d8839505bda4cb22f62ea7f329f3a294f
REIS-KIND <chikoutamu9@gmail.com>
2026-09-18 21:07:19 +0900

    CSV保存と，GAのみのBCNNも追加

--- scripts/ga_only_cifar10.py
+++ scripts/ga_only_cifar10.py
@@ -287,7 +287,7 @@ def save_csv(after_acc, after_loss):
 
 def main():
 
-    eval_size = 1000
+    eval_size = 1500
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"使用デバイス: {device}")
