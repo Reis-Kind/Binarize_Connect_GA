89d85d7ba0161e49dcdfa174f5a6d29da25ba6ac
REIS-KIND <chikoutamu9@gmail.com>
2026-09-23 01:34:53 +0900

    画像つくった

--- scripts/bin_cone_graph.py
+++ scripts/bin_cone_graph.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-CSV_FILE = "./output/ronbun/ga_only_cifar_3conv_128_500_0.0001_10000ep.csv"
+CSV_FILE = "./output/ronbun/binaryconnect_cifar_noaug_3conv_nobias_128_seed44_300ep_best_val.csv"
 
 df = pd.read_csv(CSV_FILE)
 
 # 世代データだけ取得
-ga_df = df[df["stage"] == "generation"].copy()
+ga_df = df[df["stage"] == "epoch"].copy()
 
-generations = ga_df["generation"]
+epochs = ga_df["epoch"]
 
 
 plt.figure(figsize=(12, 5))
@@ -21,20 +21,20 @@ plt.figure(figsize=(12, 5))
 plt.subplot(1, 2, 1)
 
 plt.plot(
-    generations,
+    epochs,
     ga_df["train_accuracy_percent"],
     label="Train Accuracy",
     color="black"
 )
 
 plt.plot(
-    generations,
+    epochs,
     ga_df["test_accuracy_percent"],
     label="Test Accuracy",
     color="tab:red"
 )
 
-plt.xlabel("Generation")
+plt.xlabel("epoch")
 plt.ylabel("Accuracy (%)")
 
 # 縦軸を0〜80%に固定
@@ -53,20 +53,20 @@ plt.legend()
 plt.subplot(1, 2, 2)
 
 plt.plot(
-    generations,
+    epochs,
     ga_df["train_loss"],
     label="Train Loss",
     color="black"
 )
 
 plt.plot(
-    generations,
+    epochs,
     ga_df["test_loss"],
     label="Test Loss",
     color="tab:red"
 )
 
-plt.xlabel("Generation")
+plt.xlabel("epoch")
 plt.ylabel("Loss")
 
 plt.xlim(left=0)
@@ -78,6 +78,6 @@ plt.legend()
 
 plt.tight_layout()
 
-plt.savefig("./output/ga_only_ronbun.png", dpi=300)
+plt.savefig("./output/bin_cone_ronbun.png", dpi=300)
 
 plt.show()
--- scripts/ga_bc_graph.py
+++ scripts/ga_bc_graph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-CSV_FILE = "./output/ronbun/ga_only_cifar_3conv_128_500_0.0001_10000ep.csv"
+CSV_FILE = "./output/ronbun/ga_cifar_noaug_3conv_nobias_128_BPseed44_best_val_300ep_2000batch_0.0001_1000.csv"
 
 df = pd.read_csv(CSV_FILE)
 
@@ -22,7 +22,7 @@ plt.subplot(1, 2, 1)
 
 plt.plot(
     generations,
-    ga_df["train_accuracy_percent"],
+    ga_df["sample_accuracy_percent"],
     label="Train Accuracy",
     color="black"
 )
@@ -54,7 +54,7 @@ plt.subplot(1, 2, 2)
 
 plt.plot(
     generations,
-    ga_df["train_loss"],
+    ga_df["sample_loss"],
     label="Train Loss",
     color="black"
 )
@@ -78,6 +78,6 @@ plt.legend()
 
 plt.tight_layout()
 
-plt.savefig("./output/ga_only_ronbun.png", dpi=300)
+plt.savefig("./output/ga_bc_ronbun.png", dpi=300)
 
 plt.show()
